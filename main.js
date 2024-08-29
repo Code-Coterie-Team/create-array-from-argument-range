411843efae9e2bc866b63a7d701ceacef6af509c
saeedehTorkamani <saeedehtorkamani@gmail.com>
2024-08-29 17:10:44 +0330

    #Implement new files and functions

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function creatInformationOne () {
+    const informationOne = [];
+
+    for  (let i=0; i <= 10; i++ ) {
+        informationOne.push(i);
+        
+    }
+    console.log(informationOne);
+}
+creatInformationOne();
+
+function creatInformationTwo () {
+    const informationtwo = [];
+
+    for  (let i=0; i >= -10; i-- ) {
+        informationtwo.push(i);
+        
+    }
+    console.log(informationtwo);
+}
+creatInformationTwo();
